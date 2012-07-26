db144f32442089a514b5a49e22deed13eaf9905b
Rafael Keech <Rafael.Keech@us.sogeti.com>
2012-07-26 10:16:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Add2DatabaseApp::Application.config.secret_token = '9eef99fae210021e781947f58956927d19a4997225f29874716d5cbd45fa0489db7310cd75769b07472163213f046185834bd1f2e0f08638dc52118d6a9813ae'
